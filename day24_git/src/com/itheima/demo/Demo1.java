a12bf65834cc947cfc10e57c9e197ed8ec54c574
zhangsan <zhangsan@itcast.cn>
2020-03-01 15:59:04 +0800

    提交Demo1.修改代码

--- day24_git/src/com/itheima/demo/Demo1.java
+++ day24_git/src/com/itheima/demo/Demo1.java
@@ -3,5 +3,6 @@ package com.itheima.demo;
 public class Demo1 {
     public static void main(String[] args) {
         System.out.println("hello");
+        System.out.println("我晚上修改了代码,提交远程");
     }
 }
